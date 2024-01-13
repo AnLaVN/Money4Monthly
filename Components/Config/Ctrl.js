b0c46928d6808c6b1f56f23abf4f09c33b491737
AnLaVN <binhan29122003@gmail.com>
2024-01-13 14:07:49 +0700

    Add Config component

--- Components/Config/Ctrl.js
+++ Components/Config/Ctrl.js
@@ -1,31 +1,39 @@
 app.controller("ConfigCtrl", ["$scope", "$rootScope", "$timeout", function ($scope, $rootScope, $timeout) {
 //-------------------------------------------------- Environment variable
 $scope.config = null;
+$scope.FirebaseConfig = M4M.FirebaseConfigExample;
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Environment variable
 
+
+
 //-------------------------------------------------- Logic function
+// AnLaVN - Update form changed status to [true]
+$scope.ChangeForm = function(){
+	$scope.formChanged = true;
+}
+
 // AnLaVN - Get config firebase from local function
 $scope.getConfig = function(){
-	var config = localStorage.getItem(M4M.FirebaseConfig);
-	$scope.config = config ? JSON.parse(config) : null;
+	$scope.$apply(() => $scope.config = localStorage.getItem(M4M.FirebaseConfig) || null);
 }
+
 // AnLaVN - Set config firebase to local function
 $scope.setConfig = function(){
 	localStorage.setItem(M4M.FirebaseConfig, JSON.stringify(JSON.parse($scope.config)));
 	$scope.setUpConfig();
 }
+
 // AnLaVN - Setup config firebase function
 $scope.setUpConfig = function(){
 	$scope.getConfig();
 	if($scope.config){
-		firebase.initializeApp($scope.config);	
-		M4Mdb = firebase.firestore();
+		firebase.initializeApp(JSON.parse($scope.config));	
+		M4Mfs = firebase.firestore();
 		$timeout(() => $rootScope.AddNotifis("Kết nối thành công với Firebase", "success"));
 
-		var fireStores = ["Wallet", "Income", "Sending"]
-		for (let i in fireStores) {
-			let fireStore = fireStores[i];
-			let db = M4Mdb.collection(M4M.AppName).doc(fireStore);
+		for (let i in $rootScope.M4M) {
+			let fireStore = $rootScope.M4M[i].name;
+			let db = M4Mfs.collection(M4M.AppName).doc(fireStore);
 			function onSnapshotFireStore(){
 				db.onSnapshot(newData => {
 					$scope.$apply(() => $rootScope.M4M[fireStore] = newData.data())
@@ -33,7 +41,7 @@ $scope.setUpConfig = function(){
 			}
 			db.get().then(data => {	
 				if (data.exists) onSnapshotFireStore();
-				else db.set({}).then(() => {
+				else db.set({...($rootScope.M4M[fireStore]), data: [], time: new Date()}).then(() => {
 					$timeout(() => {$rootScope.AddNotifis(`Khởi tạo kho lưu trữ ${fireStore} thành công.`, "success")}, 10);
 					onSnapshotFireStore();
 				}).catch(error => {	
