2259c819a5c456a62b5de9249a5be338cc4fc1f7
AnLaVN <binhan29122003@gmail.com>
2024-01-14 09:00:18 +0700

    Update Statistical page

--- Pages/Statistical/Ctrl.js
+++ Pages/Statistical/Ctrl.js
@@ -1,18 +1,20 @@
 app.controller("StatisticalCtrl", ["$scope", "$rootScope", "$location", "$timeout", "$filter", "$translate", function ($scope, $rootScope, $location, $timeout, $filter, $translate) {
 //-------------------------------------------------- Environment variable
 $rootScope.AppPath = $location.path().substring($location.path().lastIndexOf("/"));
+$scope.Statistical = {name: "income", groupBy: "day"}
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Environment variable
 
 
 
 //-------------------------------------------------- Logic function
-$scope.drawChart = function(e, color, groupBy){
-	let data = e.data.reduce((acc, current) => {
+$scope.drawChart = function(){
+	let data = $scope.Statistical.name == 'income' ? $rootScope.M4M.Income.data : $rootScope.M4M.Sending.data
+	data = data.reduce((acc, current) => {
 		const date = $rootScope.viewTime(current.time);
 		const year = date.getFullYear();
 		const month = date.getMonth() + 1;
 		const day = date.getDate();
-		const key = groupBy == "year" ? year : (groupBy == "month" ? `${year}-${month}` : `${year}-${month}-${day}`);
+		const key = $scope.Statistical.groupBy == "year" ? `${year}-1` : ($scope.Statistical.groupBy == "month" ? `${year}-${month}` : `${year}-${month}-${day}`);
 
 		if (acc.has(key)) acc.get(key).total += current.price * (current.category==M4M.CategoryDiscount?-1:1);
 		else acc.set(key, { time: new Date(key), total: current.price });
@@ -21,33 +23,24 @@ $scope.drawChart = function(e, color, groupBy){
 	data = Array.from(data.values());
 	data = $filter('orderBy')(data, "time")
 	
-	new Chart(document.getElementById("Chart-" + e.name).getContext("2d"), {
+	let oldChart = Chart.getChart("Statistical-Chart");
+	if (oldChart != undefined) oldChart.destroy();
+	new Chart(document.getElementById("Statistical-Chart").getContext("2d"), {
 		type: 'bar',
 		data: {
-			labels: data.map(e => $filter('date')(e.time, "dd/MM/yyyy")),
+			labels: data.map(e => $filter('date')(e.time)),
 			datasets: [
 				{
-					label: $translate.instant(e.name.toLowerCase() + ".name"),
+					label: $translate.instant($scope.Statistical.name.toLowerCase() + ".name"),
 					data: data.map(e => e.total),
 					maxBarThickness: 100,
-					backgroundColor: `rgba(${color[0]}, ${color[1]}, ${color[2]}, 0.2)`,
-					borderColor: `rgb(${color[0]}, ${color[1]}, ${color[2]})`,
+					backgroundColor: "rgba(153, 102, 255, 0.2)",
+					borderColor: "rgb(153, 102, 255)",
 					borderWidth: 1
 				}
 			]
 		}
 	});
 }
-
-$scope.AvgSending = function(){
-	$scope.drawChart($rootScope.M4M.Income, [153, 102, 255]);
-	$scope.drawChart($rootScope.M4M.Sending, [153, 102, 255]);
-}
-
-
-
-
-
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Logic function
-$timeout(()=>{$scope.AvgSending()}, 1000)
 }]);
