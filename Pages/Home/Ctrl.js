ca058fc7ce87eeab7c5d813781b5b1de9215744f
AnLaVN <binhan29122003@gmail.com>
2024-01-20 05:51:06 +0700

    Update Home page

--- Pages/Home/Ctrl.js
+++ Pages/Home/Ctrl.js
@@ -1,11 +1,8 @@
 app.controller("HomeCtrl", ["$scope", "$rootScope", "$location", function ($scope, $rootScope, $location) {
 //-------------------------------------------------- Environment variable
 $rootScope.AppPath = $location.path().substring($location.path().lastIndexOf("/"));
+$scope.Author = M4M.AppAuthor;
+$scope.AppName = M4M.AppName;
+$scope.copyright = new Date().getFullYear();
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Environment variable
-
-
-
-//-------------------------------------------------- Logic function
-
-//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Logic function
 }]);
